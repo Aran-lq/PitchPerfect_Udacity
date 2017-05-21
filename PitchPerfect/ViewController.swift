d3237a7020ad922e7a444c03346aaff7db3e7d4d
Aran-lq <liqi9281@gmail.com>
2017-05-21 19:37:47 +0800

    Fix the bug before last commit but no UI

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("please listen to me!")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("be tame! you hurt me!")
+        recordingLabel.text = "recording now"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("be tough! I can't feel it !!!")
+        recordingLabel.text = "press to record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
 }
 
