4d27c16528b0fd5b9e066619008084ee967592e3
Aran-lq <liqi9281@gmail.com>
2017-05-24 14:02:20 +0800

    connect every button and make them usable.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: NSURL!   //let it can receive the record file's path
+      @IBOutlet weak var snailButton: UIButton!
+      @IBOutlet weak var chipmunkButton: UIButton!
+      @IBOutlet weak var rabbitButton: UIButton!
+      @IBOutlet weak var vaderButton: UIButton!
+      @IBOutlet weak var echoButton: UIButton!
+      @IBOutlet weak var reverbButton: UIButton!
+      @IBOutlet weak var stopButton: UIButton!
 
+    var recordedAudioURL: URL!   //let it can receive the record file's path
+
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
